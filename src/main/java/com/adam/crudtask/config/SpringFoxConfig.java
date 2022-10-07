72a70e30e2b9f749ea65468af10b29bfe50f22ad
arashimoya <arasimovicadam@gmail.com>
2022-10-07 19:59:15 +0200

    Implemented Swagger Documentation

---
+++ src/main/java/com/adam/crudtask/config/SpringFoxConfig.java
@@ -0,0 +1,34 @@
+package com.adam.crudtask.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo("Task REST API", "API for Task App.", "1.0","",
+                new Contact("Adam Arasimowicz","arashimoya.github.io","arasimowicz.adam@hotmail.com"),
+                "Apache 2.0", "https://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList() );
+    }
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
