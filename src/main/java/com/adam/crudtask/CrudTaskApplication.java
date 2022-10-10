79be8d8e930d1e621ccb55c4df9f1bb61b39e737
arashimoya <arasimovicadam@gmail.com>
2022-10-10 20:15:20 +0200

    Implemented OAuth 2.0 Authorization

--- src/main/java/com/adam/crudtask/CrudTaskApplication.java
+++ src/main/java/com/adam/crudtask/CrudTaskApplication.java
@@ -2,12 +2,49 @@ package com.adam.crudtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
 @SpringBootApplication
-public class CrudTaskApplication {
+@PropertySource("src/main/resources/secrets.yml")
+public class CrudTaskApplication extends WebSecurityConfigurerAdapter {
+
+    @GetMapping("/user")
+    public Map<String, Object> user (@AuthenticationPrincipal OAuth2User principal){
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CrudTaskApplication.class, args);
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a-> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                ).logout(l-> l
+                        .logoutSuccessUrl("/").permitAll())
+                .csrf(c-> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .exceptionHandling(e-> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))).oauth2Login();
+    }
+
 
 }
